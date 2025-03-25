da77255f00a19a9155236dc308c6cd7e13b430e1
Vikash Kushwaha <2022pcecsvikash182@poornima.org>
2025-03-25 10:22:01 +0530

    Added Defualt_arr_ele_value.java

---
+++ VAC/Default_arr_ele_value.java
@@ -0,0 +1,11 @@
+package VAC;
+
+public class Default_arr_ele_value {
+    public static void main(String []args){
+        int a=4;
+        int arr[]=new int[4];
+        for(int i=0;i<a;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
