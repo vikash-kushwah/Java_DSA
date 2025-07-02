b1e84880b6ab1a9e03f3c41d23f9311cf5ae1307
Vikash Kushwaha <2022pcecsvikash182@poornima.org>
2025-07-02 10:34:33 +0530

    Updated scope.java

--- Practice/scope.java
+++ Practice/scope.java
@@ -1,17 +1,28 @@
 package Practice;
 
-public class swap {
-    static int a=10, b=20;
+public class scope {
+    static int a=10, b=20;   //share a single copy of variable among all objects at a class level
+
     public static void main(String[] args) {
-        {
+        int c=80;
+        System.out.println(c);
+        { //value initialised in this block will remain in this block
             int a=50,b=60;
+//            int c=100;     //we cannot declare this again
+            c=100;   //we can use and modify it or initialise it
             System.out.println(a+" "+b);
-        }
+
+            int d=30;
+        }  //scope of a,b,d ends here which are declare inside the block
+
+        int d=90; //therefore we can declare d here
+        System.out.println(c);
+
         System.out.println(a+" "+b);
-        print();
+        swap();
         System.out.println(a+" "+b);
     }
-    static void print(){
+    static void swap(){
         int temp=a;
         a=b;
         b=temp;
